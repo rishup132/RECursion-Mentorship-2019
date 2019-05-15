756e33deb342aa216fc80e5f9e614ae8afad4eeb
ISHIKA CHOWDHURY <50668776+ishika-a@users.noreply.github.com>
2019-05-16 00:33:17 +0530

    Create 3.java

---
+++ Week-1/Day-2/Ishika/3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class add
+{
+     
+     public static void main(String[] args)
+     {
+     Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt(),n=0,s=0;String str="",st="";
+        
+    for(int k=1;k<=t;k++)
+        {
+            n=sc.nextInt();
+            s=sc.nextInt();
+            System.out.println(n%s);
+            
+        }
+    }
+}
+                    
+            
+            
