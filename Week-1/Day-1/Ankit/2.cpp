a4fc5fa08f09ac6723a010674d31365e1c8eda8a
0078017191218 <45296197+0078017191218@users.noreply.github.com>
2019-05-17 14:01:45 +0530

    Update 2.cpp

--- Week-1/Day-1/Ankit/2.cpp
+++ Week-1/Day-1/Ankit/2.cpp
@@ -1,19 +1,6 @@
 include<stdio.h>
- int main()
- {
- int n1;
- scanf("%d",n1);
- for( i=1;i<=n1,i++)
- {
- int d,n,sum=0;
- scanf("%d\n",&n);
- while (n>0)
- {
- d=n%10;
- sum=sum + d;
- n=n/10;
- }
- printf("%d\n",sum);
+int main()
+{
+ printf("hello kriti");
  return 0;
- }
- 
+}
