290782df7678d4a718361b48d325ad9e9c1ecfb2
ISHIKA CHOWDHURY <50668776+ishika-a@users.noreply.github.com>
2019-05-21 01:30:39 +0530

    Create 5.cpp

---
+++ Week-2/Day-2/Ishika/5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int n,d,pos,p,c=0,s=0;
+
+cin >> n >> d;int a[n];
+for(int i=0;i<n;i++)
+	cin >>a[i];
+	for(int i=0;i<n;i++)
+	{
+		s=s+a[i];
+}
+s=s+(n-1)*10;
+
+if(s>d)
+			cout << -1;
+			else
+			{
+				p=d-s;
+				p=p/5+(n-1)*2;
+				cout <<p;
+			}
+		return 0;
+}
+→Judgement Protoc
